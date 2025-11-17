c291711db64d05f45326465ca70b2bbd54e9271b
pudit <pudit@pudits-MacBook-Air.local>
2025-11-17 14:39:35 -0500

    init web

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+import pic from './assets/pic.png'
+
+function App() {
+  return (
+    <>
+      <h2>
+        15-418/15-618: The tall, skinny QR (TSQR) decomposition
+      </h2>
+      Name: Chutchatut Sutichavengkul, Pudit Tempattarachoke
+      <br />
+      <br />
+      <img src={pic} width="500px"/>
+      <br />
+
+      proposal: &nbsp;
+      <a href="https://google.com">
+        link
+      </a>
+    </>
+  )
+}
+
+export default App
