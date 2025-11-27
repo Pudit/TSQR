93a0686b247929ed4b1ca9c6821c7d9ac1a08eeb
Chutchatut Sutichavengkul <chutchatut@gmail.com>
2025-11-27 17:38:02 -0500

    update styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
-import { useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
+import { faFilePdf } from '@fortawesome/free-solid-svg-icons'
 import './App.css'
 
 import pic from './assets/pic.png'
@@ -12,17 +14,23 @@ function App() {
       Name: Chutchatut Sutichavengkul, Pudit Tempattarachoke
       <br />
       <br />
-      <img src={pic} width="500px"/>
+      <img src={pic} width="500px" />
       <br />
 
-      Proposal: &nbsp;
       <a href="/TSQR/15_418_proposal.pdf" target="_blank">
-        link
+        <FontAwesomeIcon icon={faFilePdf} /> Proposal
       </a>
       <br />
-      Midsemester Report: &nbsp;
       <a href="/TSQR/15_418_milestone.pdf" target="_blank">
-        link
+        <FontAwesomeIcon icon={faFilePdf} /> Midsemester Report
+      </a>
+      <br />
+      <a href="https://github.com/Pudit/TSQR_IMPL/tree/OpenMP" target="_blank">
+        <FontAwesomeIcon icon={faGithub} /> OpenMP
+      </a>
+      <br />
+      <a href="https://github.com/Pudit/TSQR_IMPL/tree/OpenMPI" target="_blank">
+        <FontAwesomeIcon icon={faGithub} /> OpenMPI
       </a>
     </>
   )
